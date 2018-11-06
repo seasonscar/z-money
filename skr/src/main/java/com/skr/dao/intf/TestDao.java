56b122df707efd6bcf1f13aac8438d5878bb20cc
lalaguozidong <33615734+lalaguozidong@users.noreply.github.com>
2018-11-06 14:22:49 +0800

    Add files via upload

---
+++ skr/src/main/java/com/skr/dao/intf/TestDao.java
@@ -0,0 +1,8 @@
+package com.skr.dao.intf;
+
+/**
+ * Created by 15082188 on 2018/8/2.
+ */
+public interface TestDao {
+
+}
---
+++ skr/src/main/java/com/skr/testClass/yiweiyunsuan.java
@@ -0,0 +1,18 @@
+package com.skr.testClass;
+
+/**
+ * Created by 15082188 on 2018/10/9.
+ */
+public class yiweiyunsuan {
+    public static void main(String[] args) {
+        int a=2;
+        System.out.println(2<<1);//0010
+        System.out.println(3<<1);//0011
+        System.out.println(4<<1);//0100
+        System.out.println(5<<1);//0101
+        System.out.println(2<<2);//0010
+        System.out.println(3<<2);//0011
+        System.out.println(4<<2);//0100
+        System.out.println(5<<2);//0101
+    }
+}
